55c0b8cc62e88c5c994bac292185231cfc9d789c
Ryan Morrison <ryanmorrison@gmail.com>
2024-07-04 21:26:50 +0100

    Update Next.js export method and build settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+  }
+  
+  export default nextConfig
